0dbf518540008067182f7e83aa3f661d0255c3bd
trongle2004 <131860084+trongle2004@users.noreply.github.com>
2025-07-05 15:58:47 +0700

    Delete app directory

--- app/models/ingredients.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Float
-from app.db.database import Base
-
-class Ingredient(Base):
-    __tablename__ = "ingredients"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False)
-    stock_quantity = Column(Float, default=0)
